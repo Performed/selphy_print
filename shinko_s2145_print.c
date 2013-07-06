eed0a5c08f2d6ca8106a3a39d8c14fdaff704305
Solomon Peachy <pizza@shaftnet.org>
2013-07-06 19:08:17 -0400

    s2145:  Add a blurb about how LiveLink Technology sponsored this backend.

--- shinko_s2145_print.c
+++ shinko_s2145_print.c
@@ -3,6 +3,10 @@
  *
  *   (c) 2013 Solomon Peachy <pizza@shaftnet.org>
  *
+ *   Development of this backend was sponsored by:
+ * 
+ *     LiveLink Technology [ www.livelinktechnology.net ]
+ * 
  *   The latest version of this program can be found at:
  *
  *     http://git.shaftnet.org/git/gitweb.cgi?p=selphy_print.git
