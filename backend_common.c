49d0a0c32924788dcaf15410eab0553a877294f7
Solomon Peachy <pizza@shaftnet.org>
2018-04-10 21:18:05 -0400

    dnpds40:  Add Citizen CW-01 support to the DNP/Citizen backend.
    Supports the legacy spool format, but the intent is to move Gutenprint
    to generating the native data stream.

--- backend_common.c
+++ backend_common.c
@@ -635,7 +635,6 @@ extern struct dyesub_backend mitsu70x_backend;
 extern struct dyesub_backend mitsu9550_backend;
 extern struct dyesub_backend mitsup95d_backend;
 extern struct dyesub_backend dnpds40_backend;
-extern struct dyesub_backend cw01_backend;
 extern struct dyesub_backend magicard_backend;
 
 static struct dyesub_backend *backends[] = {
@@ -653,7 +652,6 @@ static struct dyesub_backend *backends[] = {
 	&mitsu9550_backend,
 	&mitsup95d_backend,
 	&dnpds40_backend,
-	&cw01_backend,
 	&magicard_backend,
 	NULL,
 };
